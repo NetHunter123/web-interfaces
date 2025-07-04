ed2e8bbb9a4b9fe4132d9e42f536c53d5d747d12
NetHunter <viktordmitryk3@gmail.com>
2025-07-04 13:05:25 +0300

    lab2

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,24 @@
 import Image from "next/image";
+import {Card, CardContent, CardHeader, CardTitle} from "@/components/ui/card";
+import {Button} from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <div className="App">
-          <header className="App-header">
-            <h1>Hello World!</h1> {/* Our main content */}
-            <p>
-              Welcome to your first React application.
-            </p>
-          </header>
+          <div className="flex justify-center p-4">
+            <Card className="w-full max-w-md">
+              <CardHeader>
+                <CardTitle>My Card</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <p>This card uses Shadcn UI components.</p>
+                <Button variant="default">Click Me</Button>
+              </CardContent>
+            </Card>
+          </div>
+
         </div>
 
       </main>
